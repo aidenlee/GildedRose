76ce0c3f579d883a730b3bbf805a0953556c5cfe
Aiden Lee <aiden.jihun.lee@gmail.com>
2013-02-28 20:17:11 -0500

    Refactor: Remove deep nesting and extract methods.

--- item_updater.rb
+++ item_updater.rb
@@ -4,18 +4,31 @@ class ItemUpdater
   def update(item)
     return if item.name == "Sulfuras"
 
-    item.sell_in -= 1
+    decrease_sell_in(item)
+
     if item.name == 'Aged Brie'
-      item.quality += 1
+      increase_quality(item)
     else
-      if item.sell_in < 0
-        item.quality -= 2
-      else
-        item.quality -= 1
-      end
+      decrease_quality(item)
     end
 
     item.quality = 0 if item.quality < 0
     item.quality = 50 if item.quality > 50
+  end
+
+  def decrease_quality(item)
+    if item.sell_in < 0
+      item.quality -= 2
+    else
+      item.quality -= 1
+    end
+  end
+
+  def increase_quality(item)
+    item.quality += 1
+  end
+
+  def decrease_sell_in(item)
+    item.sell_in -= 1
   end
 end
