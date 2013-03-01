8af68f4793c9ac4947bd925eda48fd46a1d5cb5e
Aiden Lee <aiden.jihun.lee@gmail.com>
2013-02-28 20:33:44 -0500

    Clean up tests.

--- gilded_rose_test.rb
+++ gilded_rose_test.rb
@@ -4,52 +4,51 @@ require 'redgreen'
 
 describe ItemUpdater do
 
-  it "decreases sell by on update" do
-    @item = Item.new("foo", 5, 10)
+  def update_item(item_config)
+    @item = Item.new(item_config[:name], item_config[:sell_in], item_config[:quality])
     ItemUpdater.new.update(@item)
+  end
+
+  it "decreases sell by on update" do
+    update_item sell_in: 5, quality: 10
+
     assert_equal 4, @item.sell_in
   end
 
   it "decreases quality value by one every day" do
-    @item = Item.new("foo", 5, 10)
-    ItemUpdater.new.update(@item)
+    update_item sell_in: 5, quality: 10
+
     assert_equal 9, @item.quality
   end
 
   it "Once the sell by date has passed, Quality degrades twice as fast" do
-    @item = Item.new("foo", -1, 10)
-    ItemUpdater.new.update(@item)
+    update_item sell_in: -1, quality: 10
+
     assert_equal 8, @item.quality
   end
 
   it "The Quality of an item is never negative" do
-    @item = Item.new("foo", 1, 2)
-    ItemUpdater.new.update(@item)
-    ItemUpdater.new.update(@item)
+    update_item sell_in: 1, quality: 0
 
     assert_equal 0, @item.quality
   end
 
   it "Aged Brie actually increases in Quality the older it gets" do
-    @item = Item.new("Aged Brie", 10, 2)
-    ItemUpdater.new.update(@item)
+    update_item name: 'Aged Brie', sell_in: 10, quality: 2
 
     assert_equal 3, @item.quality
   end
 
   it "The Quality of an item is never more than 50" do
-    @item = Item.new("Aged Brie", 10, 50)
-    ItemUpdater.new.update(@item)
+    update_item name: 'Aged Brie', sell_in: 10, quality: 50
 
     assert_equal 50, @item.quality
   end
 
   it "Sulfuras, being a legendary item, never has to be sold or decreases in Quality" do
-    @item = Item.new("Sulfuras", 0, 50)
-    ItemUpdater.new.update(@item)
+    update_item name: 'Sulfuras', sell_in: 0, quality: 50
 
     assert_equal 50, @item.quality
     assert_equal 0, @item.sell_in
   end
-
 end
